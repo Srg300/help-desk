05e85a65b2014c8e61bebc3d72f6f79aa25166ab
Srg <srg@MacAir.local>
2023-11-12 13:59:37 +0300

    небольшие изменения в настройках алхимии к БД

--- src/app/db/base/engine.py
+++ src/app/db/base/engine.py
@@ -4,17 +4,25 @@ from settings import DatabaseSettings, get_settings
 
 _settings = get_settings(DatabaseSettings)
 
-async_engine = create_async_engine(
-    _settings.url,
-    future=True,
-    pool_size=20,
-    pool_pre_ping=True,
-    pool_use_lifo=True,
+
+class DBHelper:
+    def __init__(self, *, url: str, echo: bool = False) -> None:
+        self.async_engine = create_async_engine(
+            url=url,
+            future=True,
+            pool_size=20,
+            pool_pre_ping=True,
+            pool_use_lifo=True,
+            echo=echo,
+        )
+
+        self.async_session_factory = async_sessionmaker(
+            bind=self.async_engine,
+            expire_on_commit=False,
+        )
+
+
+db_helper = DBHelper(
+    url=_settings.url,
     echo=_settings.echo,
-)
-
-
-async_session_factory = async_sessionmaker(
-    bind=async_engine,
-    expire_on_commit=False,
 )
