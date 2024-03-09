f319a2ad1120eef568b0b3a045bd0d14cb39bab8
Srg <srg@MacAir.local>
2024-03-09 13:40:54 +0300

    Edit dto and schema

--- src/core/domain/user/dto.py
+++ src/core/domain/user/dto.py
@@ -1,9 +1,18 @@
+from pydantic import EmailStr, computed_field
+
 from core.dto import BaseDto
+from core.security import get_password_hash
 
 
 class UserCreateDto(BaseDto):
-    email: str
+    email: EmailStr
     password: str
+    username: str
+
+    @property
+    @computed_field
+    def hashed_password(self) -> str:
+        return get_password_hash(password=self.password)  # type: ignore[no-any-return]
 
 
 class UserSimpleOutDto(BaseDto):
